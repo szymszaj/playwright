34e45b63e360fe30ca2cb8a65d4513d284db1bf1
Szymon Zych <szymonzych868@gmail.com>
2024-04-08 12:52:22 +0200

    implementation of test dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
+import baseEnvUrl from "./untils/environmentBaseUrl";
 
 /**
  * Read environment variables from file.
@@ -10,7 +11,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -20,33 +21,82 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        // viewport: { width: 1280, height: 720 },
+      },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
+    {
+      name: "all-browsers-and-tests",
+      use: {
+        baseURL: "https://playwright.dev/",
+        ...devices["Desktop Chrome"],
+      },
+    },
+
+    {
+      name: "all-browsers-and-tests",
+      use: {
+        baseURL: "https://playwright.dev/",
+        ...devices["Desktop Safari"],
+      },
+    },
+
+    {
+      name: "all-browsers-and-tests",
+      use: {
+        baseURL: "https://playwright.dev/",
+        ...devices["Desktop Firefox"],
+      },
+    },
+    // Example only
+    {
+      name: "local",
+      use: {
+        baseURL: baseEnvUrl.local.home,
+      },
+    },
+
+    // Example only
+    {
+      name: "ci",
+      use: {
+        baseURL: process.env.CI
+          ? baseEnvUrl.ci.prefix +
+            process.env.GITHUB_REF_NAME +
+            baseEnvUrl.ci.suffix //https://dev-myapp-chapter-2.mydomain.com
+          : baseEnvUrl.staging.home,
+      },
+      /**
+       * GitHub variables: https://docs.github.com/en/actions/learn-github-actions/variables
+       * GitLab variables: https://docs.gitlab.com/ee/ci/variables/predefined_variables.html#predefined-variables-reference
+       */
+    },
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
